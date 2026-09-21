b41651333668c2c73bd96123ffd7e1ef457a8979
lily <eshigh23@gmail.com>
2026-09-21 16:30:12 -0400

    Enabled cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
+
+
 
 const app = express()
 const port = 3000;
 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
 
 
 app.get('/api/status', (req, res) => {
